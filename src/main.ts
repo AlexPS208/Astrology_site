8a7e73b611983cec3488ffd52f59ecb27b512b88
Alexander <91263783+Twilight-Al-Sparkle@users.noreply.github.com>
2023-04-21 01:58:05 +0300

    Try to debug deploy

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import dotenv from 'dotenv/config';
-import ejs from 'ejs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
-  app.useStaticAssets(join(__dirname, '..', 'views'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('ejs');
+  // app.useStaticAssets(join(__dirname, '..', 'views'));
+  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // app.setViewEngine('ejs');
+  app.set('views', join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
   const PORT = process.env.PORT || 3000
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+  });
 }
 bootstrap();
