e77b1bb026231c5cc7b94edd212688acde6dbbc6
Alexander <91263783+Twilight-Al-Sparkle@users.noreply.github.com>
2023-04-21 02:03:20 +0300

    Dicz

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import dotenv from 'dotenv/config';
+import ejs from 'ejs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -14,6 +15,7 @@ async function bootstrap() {
   // app.setViewEngine('ejs');
   app.set('views', join(__dirname, 'views'));
   app.set('view engine', 'ejs');
+  app.engine('ejs', ejs.__express);
 
   const PORT = process.env.PORT || 3000
   await app.listen(PORT, () => {
